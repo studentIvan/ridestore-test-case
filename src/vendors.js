a149b7ac70f38e43fb721610bf60b9eb9ebc676c
Ivan Maslov <cryptox3@gmail.com>
2017-08-13 16:38:23 +0400

    Add angular-resource to vendors

--- src/vendors.js
+++ src/vendors.js
@@ -2,5 +2,6 @@
  * Separated vendors file helps us to collect all vendors without unnecessary rebuild
  */
 import angular from 'angular';
+import ngResource from 'angular-resource';
 
-export { angular };
+export { angular, ngResource };
