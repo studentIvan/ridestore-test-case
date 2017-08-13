796e47364d13570fddaca58f4315b44fa60e6a14
Ivan Maslov <cryptox3@gmail.com>
2017-08-13 16:37:56 +0400

    Set arrow-parens eslint rule to error always

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   ],
   rules: {
     'angular/no-service-method': 0,
-    'object-curly-spacing': ['error', 'always']
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-parens': ['error', 'always'],
   }
 }
