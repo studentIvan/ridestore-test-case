7e994672a06a782d6b32bf78971b5e3bbab0bd54
Ivan Maslov <cryptox3@gmail.com>
2017-08-13 16:50:29 +0400

    Add header and products list to AppComponent
    Header is simple bootstrap nav covered with header tag.
    
    Component ridestore-products-list using to display the responsive list of products from category with id 208.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -3,6 +3,14 @@
  */
 export const AppComponent = {
   template: `
-    <h1>Hello World!</h1>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Ridestore</a>
+      </nav>
+    </header>
+    <div class="container-fluid">
+      <h1>Hello World!</h1>
+      <ridestore-products-list category-id="208"></ridestore-products-list>
+    </div>
   `
 };
