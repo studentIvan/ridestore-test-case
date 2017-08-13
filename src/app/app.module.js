58bb74a8fce045ec55b7818345d1599b51abdcb0
Ivan Maslov <cryptox3@gmail.com>
2017-08-13 16:48:10 +0400

    Include RidestoreProductsListModule to AppModule dependencies

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,11 +1,12 @@
 import { angular } from '../vendors';
 import { AppComponent } from './app.component';
+import { RidestoreProductsListModule } from './rs-products-list/rs-products-list.module';
 import './app.scss';
 
 /**
  * General application module
  */
 export const AppModule = angular
-  .module('app', [])
+  .module('app', [RidestoreProductsListModule])
   .component('app', AppComponent)
   .name;
